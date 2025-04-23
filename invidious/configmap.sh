bfb71d147914626324005cc3d00e3aa99881832c
root <acheong@duti.dev>
2025-04-23 21:17:48 +0530

    monitoring and anubis

--- invidious/configmap.sh
+++ invidious/configmap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kubectl delete configmap --namespace invidious \
-  init-script sql-config invidious-companion-config
+  init-script sql-config invidious-companion-config invidious-config
 kubectl create configmap init-script \
   --namespace=invidious \
   --from-file=config/init-invidious-db.sh
@@ -10,3 +10,6 @@ kubectl create configmap sql-config \
 kubectl create configmap invidious-companion-config \
   --namespace=invidious \
   --from-file=config/companion.toml
+kubectl create configmap invidious-config \
+  --namespace=invidious \
+  --from-file=config/config.yml
---
+++ monitoring/README.md
@@ -0,0 +1,4 @@
+```
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm upgrade --install -f values.yaml kube-prometheus-stack prometheus-community/kube-prometheus-stack -n monitoring
+```
